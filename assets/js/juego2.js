4eb8660a31731fc8e60e0509882c685a81369ea5
Alejandra9878 <aleja1305cano@gmail.com>
2023-10-18 22:27:26 -0600

    formulariologin

---
+++ assets/js/juego2.js
@@ -0,0 +1,20 @@
+const formulario = document.getElementById("miFormulario");
+        formulario.addEventListener("submit", function(event) {
+            event.preventDefault(); // Evita que la página se recargue al enviar el formulario
+            
+            // Capturamos los valores de los campos de entrada
+            const nombre = document.getElementById("nombre").value;
+            const contrasena = document.getElementById("contrasena").value;
+            const correo = document.getElementById("correo").value;
+
+            // Mostramos los valores en la consola
+            console.log("Nombre: " + nombre);
+            console.log("Contraseña: " + contrasena);
+            console.log("Correo: " + correo);
+        });
+    
+
+
+
+
+
